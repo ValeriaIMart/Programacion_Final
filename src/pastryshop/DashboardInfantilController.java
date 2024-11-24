6231d404adda8104cc7b2f281350da08e932c184
pccas <pccas@PCCASAEST.bwrouter>
2024-11-24 15:38:48 -0500

    Menu completo de pasteles

--- src/pastryshop/DashboardInfantilController.java
+++ src/pastryshop/DashboardInfantilController.java
@@ -4,12 +4,17 @@
  */
 package pastryshop;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -26,8 +31,94 @@ public class DashboardInfantilController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    
+    
      @FXML
-    private void Open_Dashboard(ActionEvent event) {
+    private void Open_Dashboard(ActionEvent event) throws IOException {
+           /*Abrir una ventana*/
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Dashboard");
+        stage.show();
+
+        /*Cerrar la ventana*/
+      Node source2 = (Node) event.getSource();
+      Stage stage2 = (Stage) source2.getScene().getWindow();
+      stage2.close();
+          
     }
     
+    
+        @FXML
+    private void Open_Pasteltemporada(ActionEvent event) throws IOException {
+        /*Abrir una ventana*/    
+        Stage stage=new Stage();
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("Dashboardtemporada.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Dashboardtemporada");
+        stage.show();
+        
+        /*Cerrar la ventana*/
+        Node source2 = (Node) event.getSource();
+        Stage stage2 = (Stage) source2.getScene().getWindow();
+        stage2.close();        
+        
+    }
+    @FXML
+    private void Open_PastelBoda(ActionEvent event) throws IOException {
+        /*Abrir una ventana*/    
+        Stage stage=new Stage();
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("DashboardBoda.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("DashboardBoda");
+        stage.show();
+        
+        /*Cerrar la ventana*/
+        Node source2 = (Node) event.getSource();
+        Stage stage2 = (Stage) source2.getScene().getWindow();
+        stage2.close();        
+        
+    }
+    @FXML
+    private void Open_PastelInfantil(ActionEvent event) throws IOException {
+        /*Abrir una ventana*/    
+        Stage stage=new Stage();
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("DashboardInfantil.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("DashboardInfantil");
+        stage.show();
+        
+        /*Cerrar la ventana*/
+        Node source2 = (Node) event.getSource();
+        Stage stage2 = (Stage) source2.getScene().getWindow();
+        stage2.close();        
+        
+    }
+      @FXML
+    private void Open_PastelCumple(ActionEvent event) throws IOException {
+        /*Abrir una ventana*/    
+        Stage stage=new Stage();
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("DashboardCumple.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("DashboardCumple");
+        stage.show();
+        
+        /*Cerrar la ventana*/
+        Node source2 = (Node) event.getSource();
+        Stage stage2 = (Stage) source2.getScene().getWindow();
+        stage2.close();        
+        
+    }
+    
+        @FXML
+    private void close(ActionEvent event) {
+        System.exit(0);
+    }
 }
--- src/pastryshop/DashboardtemporadaController.java
+++ src/pastryshop/DashboardtemporadaController.java
@@ -4,12 +4,17 @@
  */
 package pastryshop;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -30,7 +35,26 @@ public class DashboardtemporadaController implements Initializable {
     }    
 
     @FXML
-    private void Open_Dashboard(ActionEvent event) {
+    private void Open_Dashboard(ActionEvent event) throws IOException {
+         /*Abrir una ventana*/
+        Stage stage4 = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
+        Scene scene2 = new Scene(fxmlLoader.load());
+        stage4.setScene(scene2);
+        stage4.setTitle("Dashboard");
+        stage4.show();
+
+        /*Cerrar la ventana*/
+      Node source2 = (Node) event.getSource();
+      Stage stage = (Stage) source2.getScene().getWindow();
+      stage.close();
+          
+    }
+    
+    
+        @FXML
+    private void close(ActionEvent event) {
+        System.exit(0);
     }
     
 }
